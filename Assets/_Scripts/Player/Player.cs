e4e648bc51f113606130e924e3dc943d64e8f415
Praaug <f1clan@web.de>
2017-01-21 14:12:21 +0100

    - added player fields

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -12,6 +12,15 @@ public partial class Player
     public int elePtsWater { get; private set; }
     public int elePtsStorm { get; private set; }
     public int elePtsEarth { get; private set; }
+
+    public int health { get; private set; }
+
+    public Dictionary<EnemyType, int> enemyCountDict { get { return m_enemyCountDict; } }
+    #endregion
+
+    #region Fields
+    private Dictionary<EnemyType, int> m_enemyCountDict;
+    private Dictionary<EnemyType, int> m_enemyStrenghDict;
     #endregion
 
     #region Constructor
@@ -19,6 +28,18 @@ public partial class Player
     {
         gameObject = p_avatarInstance;
         config = p_avatarInstance.GetComponent<PlayerConfig>();
+
+        m_enemyCountDict = new Dictionary<EnemyType, int>();
+        m_enemyCountDict.Add( EnemyType.Fire, 0 );
+        m_enemyCountDict.Add( EnemyType.Water, 0 );
+        m_enemyCountDict.Add( EnemyType.Wind, 0 );
+        m_enemyCountDict.Add( EnemyType.Dirt, 0 );
+
+        m_enemyStrenghDict = new Dictionary<EnemyType, int>();
+        m_enemyStrenghDict.Add( EnemyType.Fire, MIN_DAMAGE );
+        m_enemyStrenghDict.Add( EnemyType.Water, MIN_DAMAGE );
+        m_enemyStrenghDict.Add( EnemyType.Wind, MIN_DAMAGE );
+        m_enemyStrenghDict.Add( EnemyType.Dirt, MIN_DAMAGE );
     }
 
     public void ReduceLife( int p_amount )
@@ -31,6 +52,8 @@ public partial class Player
 public partial class Player
 {
     public const int PLAYER_COUNT = 2;
+    public const int MAX_HEALTH = 10;
+    public const int MIN_DAMAGE = 100;
 
     public static List<Player> allPlayer { get; private set; }
 
