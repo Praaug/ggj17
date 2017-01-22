000700e3f8426081faa094398d3967b05bd77b5c
Praaug <f1clan@web.de>
2017-01-22 13:59:14 +0100

    no message

---
+++ Assets/_Scripts/Editor/NewMonoBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Helper : Editor
+{
+    public void SetMass()
+    {
+        foreach ( var _go in Selection.gameObjects )
+        {
+            var _rbs = _go.GetComponentsInChildren<Rigidbody>( true );
+
+            foreach ( var _rb in _rbs )
+            {
+                _rb.mass = 100;
+            }
+        }
+    }
+
+}
